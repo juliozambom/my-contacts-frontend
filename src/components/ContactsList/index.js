d1776801412b491e6ece949d88c54df4ca5e37fb
Julio Zambom <work.juliozambom@gmail.com>
2022-08-14 00:23:16 -0300

    feat: creating contacts list

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,7 @@
-import { Container, Header } from './styles';
+import { Container, Header, ListContainer, Card } from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +10,72 @@ export default function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Julio Zambom</strong>
+              <small>instagram</small>
+            </div>
+            <span>juliozamboom@gmail.com</span>
+            <span>(11) 98225-3516</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Julio Zambom</strong>
+              <small>instagram</small>
+            </div>
+            <span>juliozamboom@gmail.com</span>
+            <span>(11) 98225-3516</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Julio Zambom</strong>
+              <small>instagram</small>
+            </div>
+            <span>juliozamboom@gmail.com</span>
+            <span>(11) 98225-3516</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
